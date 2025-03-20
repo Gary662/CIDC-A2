967c455368508c340ddaf6e0ed015f3c76930564
Gary662 <garysimwawa25@gmail.com>
2025-03-19 20:44:17 -0600

    Added unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders name on page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameElement = screen.getByText(/Gary Simwawa/i);
+  expect(nameElement).toBeInTheDocument();
 });
